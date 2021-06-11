b875d9c7cfda85879a61b9848ff2a4da79c68341
Daniel Luigi <luigi.d.smmc@cs.colman.ac.il>
2021-06-11 19:45:30 +0300

    Fixed popover not showing; Edited all pages Titles

--- FeedMe/wwwroot/js/site.js
+++ FeedMe/wwwroot/js/site.js
@@ -20,10 +20,6 @@ const options = {
 darkmode.showWidget();*/
 
 
-var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
-var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
-    return new bootstrap.Popover(popoverTriggerEl)
-})
 
 /* -- quantity box in dish-- */
 
@@ -132,7 +128,9 @@ function addToCart(id, quantity) {
 }
 
 
-
+var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+    return new bootstrap.Popover(popoverTriggerEl)
 })
 
 var popover = new bootstrap.Popover(document.querySelector('.popover-dismiss'), {
